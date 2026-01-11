72ed56ff09b3a44f996c80366e566d5292f133b9
Jalaj Goswami <jalajgoswami33@gmail.com>
2026-01-11 14:25:50 +0000

    project reset

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/DataVisualization',
-    ()=> console.log('Connected to DB'),
-    (err)=> console.log(err)
-)
-
-module.exports = mongoose
